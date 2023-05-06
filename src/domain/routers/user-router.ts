43dc13b8a2e83886237c4ca2466409f635be3b95
RuannaC-137 <ruanlons@gmail.com>
2023-05-06 16:11:13 -0300

    test/find-all-user

--- src/domain/routers/user-router.ts
+++ src/domain/routers/user-router.ts
@@ -1,6 +1,7 @@
 import { Router, type Request, type Response } from 'express'
 import { findUserByIdController } from '../controllers/find-user/find-user-byId'
 import { findUsersByFieldController } from '../controllers/find-user/find-users-byField'
+import { findAllUserController } from '../controllers/find-user/find-all-users'
 import { createUserController } from '../controllers/create-user'
 import { updateUserByIdController } from '../controllers/update-user/update-user-byId'
 import { deleteUserByIdController } from '../controllers/delete-user/delete-user-byId'
@@ -14,13 +15,12 @@ router.post('/', (request: Request, response: Response) => {
 router.get('/query', (request: Request, response: Response) => {
   void findUsersByFieldController.handle(request, response)
 })
-
 router.get('/:id', (request: Request, response: Response) => {
   void findUserByIdController.handle(request, response)
 })
-/* router.get('/', (request: Request, response: Response) => {
-  void findUserByIdController.handle(request, response)
-}) */
+router.get('/', (request: Request, response: Response) => {
+  void findAllUserController.handle(request, response)
+})
 
 router.put('/:id', (request: Request, response: Response) => {
   void updateUserByIdController.handle(request, response)
